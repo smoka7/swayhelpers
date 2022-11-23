9c2af4604245f1bf6c834fd6ecc9de19174cfedc
mohsen <smoka7@hotmail.com>
2022-11-23 15:01:50 +0330

    added fast focus

--- go.mod
+++ go.mod
@@ -1,12 +1,17 @@
 module github.com/smoka7/swayHelper
 
-go 1.17
-
-require github.com/joshuarubin/go-sway v1.0.0
+go 1.19
 
 require (
-	github.com/joshuarubin/lifecycle v1.0.0 // indirect
-	go.uber.org/atomic v1.3.2 // indirect
-	go.uber.org/multierr v1.1.0 // indirect
-	golang.org/x/sync v0.0.0-20190412183630-56d357773e84 // indirect
+	github.com/joshuarubin/go-sway v1.2.0
+	golang.org/x/term v0.2.0
+)
+
+require golang.org/x/sys v0.2.0 // indirect
+
+require (
+	github.com/joshuarubin/lifecycle v1.1.4 // indirect
+	go.uber.org/atomic v1.10.0 // indirect
+	go.uber.org/multierr v1.8.0 // indirect
+	golang.org/x/sync v0.1.0 // indirect
 )
---
+++ src/fastFocus.go
@@ -0,0 +1,102 @@
+package src
+
+import (
+	"bufio"
+	"context"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+	"unicode"
+
+	"github.com/joshuarubin/go-sway"
+	"golang.org/x/term"
+)
+
+const selectorChars = "asdfgqwertzxcvbhy"
+
+const (
+	BNRM = "\x1B[40m"
+	BRED = "\x1B[41m"
+	BGRN = "\x1B[42m"
+	BYEL = "\x1B[43m"
+	BBLU = "\x1B[44m"
+	BMAG = "\x1B[45m"
+	BCYN = "\x1B[46m"
+	BWHT = "\x1B[47m"
+	FNRM = "\x1B[0m"
+	FRED = "\x1B[31m"
+	FGRN = "\x1B[32m"
+	FYEL = "\x1B[33m"
+	FBLU = "\x1B[34m"
+	FMAG = "\x1B[35m"
+	FCYN = "\x1B[36m"
+	FWHT = "\x1B[37m"
+)
+
+var containers []*sway.Node
+
+func (c Client) FastFocus() {
+	c.getContainters()
+
+	showContainers()
+
+	_, err := term.MakeRaw(0)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	c.readInput()
+}
+
+// reads the input when key is valid focusses that container
+// or else waits for valid key none letter keys terminates the program
+func (c Client) readInput() {
+	in := bufio.NewReader(os.Stdin)
+	for {
+		r, _, err := in.ReadRune()
+		if err != nil {
+			log.Fatalln("stdin:", err)
+		}
+
+		if !unicode.IsLetter(r) && !unicode.IsNumber(r) {
+			break
+		}
+
+		c.goToContainer(r)
+
+	}
+}
+
+// getContainters gets the focused workspace containers
+func (c Client) getContainters() {
+	focusedWS := c.getFocusedWs()
+	containers = getChildNodes(focusedWS.Nodes)
+	containers = append(containers, getChildNodes(focusedWS.FloatingNodes)...)
+}
+
+// TODO maybe show Gui
+func showContainers() {
+	for index, container := range containers {
+		fmt.Printf(" %s %s %s %s %s\n\n", BRED, FYEL, string(selectorChars[index]), FNRM, container.Name)
+	}
+
+	fmt.Printf("%s %s", BNRM, FNRM)
+}
+
+// goToContainer switches the focus to the node that r represents
+func (c Client) goToContainer(r rune) {
+	index := strings.IndexRune(selectorChars, r)
+	if index < 0 || index >= len(containers) {
+		return
+	}
+
+	command := fmt.Sprintf("[con_id=%d] focus", containers[index].ID)
+	_, err := c.Conn.RunCommand(context.Background(), command)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	os.Exit(0)
+}
